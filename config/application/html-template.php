332c35fe82bced41d4e17c0fd55b7ef8c41051fd
Richardson Allan Ferreira de Souza <richardson.allan.souza@gmail.com>
2024-07-02 19:19:37 -0400

    chore: update config files

---
+++ config/application/html-template.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'templates' => [
+        'extension' => 'phtml',
+        'paths'     => [
+            'app'      => ['templates/app'],
+            'error'    => ['templates/error'],
+            'layout'   => ['templates/layout'],
+            'partials' => ['templates/partials'],
+        ],
+    ],
+];
---
+++ config/dependencies/application.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'invokables' => [
+        App\Application\Rest\Handler\V1\PingHandler::class => App\Application\Rest\Handler\V1\PingHandler::class,
+    ],
+    'factories'  => [
+    ],
+];
